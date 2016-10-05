52039c1168abd2ae93df93c18e8db548f534ffbf
Jayden Bailey <jaydenkieran@gmail.com>
2016-10-05 14:21:10 +0100

    support delete event

--- src/lib/fire.js
+++ src/lib/fire.js
@@ -211,3 +211,30 @@ exports.create = (req, res, next) => {
     });
   });
 };
+
+/**
+ * Fire webhooks for a delete event.
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ */
+exports.delete = (req, res, next) => {
+  // Generate the webhook body
+  let webhookBody = Mustache.render(templates['delete_' + req.body.ref_type], req.body);
+
+  // Send the webhooks
+  sendToDiscord(req.hook.discordWebhooks, webhookBody).then(() => {
+    res.send({
+      message: 'OK',
+      hint: 'Sent data successfully to ' + req.hook.discordWebhooks.length + ' hooks.'
+    });
+  }).catch(err => {
+    console.error('Failed to run hook on ' + req.body.repository.full_name + ':');
+    console.error(err);
+
+    res.status(500).send({
+      message: 'InternalServerError',
+      error: err
+    });
+  });
+};
