0c545d31693999bbd835e6368ef049220e8831f6
Jayden Bailey <jaydenkieran@gmail.com>
2016-10-05 13:42:06 +0100

    support commit_comment event

--- src/lib/fire.js
+++ src/lib/fire.js
@@ -144,3 +144,35 @@ exports.watch = (req, res, next) => {
     });
   });
 };
+
+/**
+ * Fire webhooks for a commit_comment event.
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ */
+exports.commit_comment = (req, res, next) => {
+  // Generate the webhook body
+  req.body.comment.sha = req.body.comment.commit_id.substr(0, 7);
+  req.body.comment.message = req.body.comment.body.replace(/\r?\n|\r/g, " ").substr(0, 200).trim();
+  if (req.body.comment.body.length > 200) {
+    req.body.comment.message += "..."
+  }
+  let webhookBody = Mustache.render(templates.commit_comment, req.body);
+
+  // Send the webhooks
+  sendToDiscord(req.hook.discordWebhooks, webhookBody).then(() => {
+    res.send({
+      message: 'OK',
+      hint: 'Sent data successfully to ' + req.hook.discordWebhooks.length + ' hooks.'
+    });
+  }).catch(err => {
+    console.error('Failed to run hook on ' + req.body.repository.full_name + ':');
+    console.error(err);
+
+    res.status(500).send({
+      message: 'InternalServerError',
+      error: err
+    });
+  });
+};
